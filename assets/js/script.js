f3e69ddf061904a1d7e5f080b1174cc22d7bb50b
Tez28 <courtezcannady@gmail.com>
2023-04-17 16:59:57 -0400

    save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
     };
 
     alert("Task Updated!");
-
+    saveTasks();
     // remove data attribute from form
     formEl.removeAttribute("data-task-id");
     // update formEl button to go back to saying "Add Task" instead of "Edit Task"
@@ -92,6 +92,8 @@ var createTaskEl = function(taskDataObj) {
 
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
+
+    saveTasks();
     // increase task counter for next unique id
     taskIdCounter++;
 
@@ -182,6 +184,8 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
+
+    saveTasks();
 };
 var editTask = function(taskId) {
     console.log("editing task #" + taskId);
@@ -220,8 +224,12 @@ var deleteTask = function(taskId) {
     }
     // reassign tasksarray to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 };
 
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 // create new task
 formEl.addEventListener("submit", taskFormHandler);
 
